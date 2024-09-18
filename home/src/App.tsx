cf61c4481950d584720d6285451f50c011a84546
wan <wandersonsales.dev@gmail.com>
2024-09-18 07:42:32 -0300

    :heavy_plus_sign: Add style-components

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -1,19 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
+import "./index.scss";
+import HomePage from "./pages/home.page";
 
-const App = () => (
-  <div className="container">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Empty CSS</div>
-  </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+const App = () => <HomePage />;
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-root.render(<App />)
+root.render(<App />);
